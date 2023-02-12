aa84020ce880ca032de0aadf952d31c73cb78ca8
npxhui <npxhui@gmail.com>
2023-02-12 13:43:57 +0800

    chapterOne1_4函数内联

--- ChapterOne/1_4.js
+++ ChapterOne/1_4.js
@@ -1,23 +1,29 @@
-function amountFor(perf, play) {
-    let thisAmount = 0;
-    switch (play.type) {
+// 内联
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+// 提炼函数
+function amountFor(aPerformance) { // aPerformance跟踪变量类型
+    let result = 0; // 重命名
+    switch (playFor(aPerformance).type) {
         case "tragedy":
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience -20);
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`Unknown type: $(play.type)`);
+            throw new Error(`Unknown type: $(playFor(aPerformance).type)`);
     }
-    return thisAmount;
+    return result;
 }
 
 function statement (invoice, plays) {
@@ -26,17 +32,14 @@ function statement (invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat("en-US", {style:"currency", currency:"USD", minimumIntegerDigits: 2}).format;
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
-        
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)`;
-        totalAmount += thisAmount;
+        result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)`;
+        totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
